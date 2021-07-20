fe17300e84444bd830dea3f50818053b17b18170
Hidalgo Ch Ponce <hidalgoponce@icloud.com>
2021-07-20 17:54:45 -0500

    nueva plantilla y crear link active por AppServiceProvider

--- routes/instructor.php
+++ routes/instructor.php
@@ -3,6 +3,7 @@
 
 use App\Http\Controllers\Instructor\CourseController;
 use App\Http\Livewire\CoursesIndex;
+use App\Http\Livewire\Instructor\CoursesCurriculum;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('', '/instructor/courses');
@@ -10,3 +11,5 @@ Route::redirect('', '/instructor/courses');
 //Route::get('courses', CoursesIndex::class)->middleware('can:Leer cursos')->name('courses.index');
 
 Route::resource('courses', CourseController::class)->names('courses');
+
+Route::get('courses/{course}/curriculum', CoursesCurriculum::class)->name('courses.curriculum');
