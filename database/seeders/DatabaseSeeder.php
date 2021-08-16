f5600f1ed0e454101faad02ae4bb22fb02635417
Hidalgo Ch Ponce <hidalgoponce@icloud.com>
2021-08-16 08:55:19 -0500

    Fusion realizada con la nueva conexion reota

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,16 +17,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        Storage::deleteDirectory('courses');
-        Storage::makeDirectory('courses');
+//        Storage::deleteDirectory('courses');
+//        Storage::makeDirectory('courses');
 
         $this->call(PermissionSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
-        $this->call(LevelSeeder::class);
-        $this->call(CategorySeeder::class);
-        $this->call(PriceSeeder::class);
-        $this->call(PlatformSeeder::class);
-        $this->call(CourseSeeder::class);
+//        $this->call(LevelSeeder::class);
+//        $this->call(CategorySeeder::class);
+//        $this->call(PriceSeeder::class);
+//        $this->call(PlatformSeeder::class);
+//        $this->call(CourseSeeder::class);
     }
 }
