936a4813c2a7de52d613bc55c834796537af32b3
Hidalgo Ch Ponce <hidalgoponce@icloud.com>
2021-08-17 18:38:34 -0500

    Update

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -108,7 +108,8 @@
                             @auth
                                 <form action="{{route('courses.enrolled', $course)}}" method="post">
                                     @csrf
-                                    <button type="submit"  class="btn-danger btn-block">Llevar este cursos</button>
+{{--                                    <button type="submit"  class="btn-danger btn-block">Llevar este cursos</button>--}}
+                                    <button type="submit"  class="btn-danger btn-block">REGISTRO PARA CERTIFICACIÓN</button>
                                 </form>
                             @else
 {{--                                <button type="button" class="btn-danger mt-2 btn-block" x-on:click="open = true" x-show="open">Matricularme a este curso</button>--}}
